1c62b749dc72706eadda0111370a2252a0edab59
Gnak <523748995@qq.com>
2024-08-13 14:48:55 +0800

    feat: 增加umami数据统计

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import { getAllPosts } from '../lib/api.js';
 import Blog_list from '../components/blog_list/Blog_list.js';
 import Profile from '../components/profile/Profile.js';
+import Head from 'next/head.js'
 
 export default function Home({ allPosts, theme, config }) {
   return (
+    <>
+     <Head>
+     <script defer src="https://cloud.umami.is/script.js" data-website-id="ec5537b7-7698-4b5b-87a1-a5fa41855086"></script>                      
+     </Head>
     <div className="fade-in">
       <Profile config={config} />
       <Blog_list allPosts={allPosts} theme={theme} config={config} />
     </div>
+    </>
+    
   );
 }
 export const getStaticProps = async () => {
